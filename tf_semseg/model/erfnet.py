87699fefb14d4f8755dd1173302cfcfc3f187024
Florian Fervers <florian.fervers@gmail.com>
2021-06-13 14:01:01 +0200

    Make erfnet nd

--- tf_semseg/model/erfnet.py
+++ tf_semseg/model/erfnet.py
@@ -9,20 +9,28 @@ def non_bottleneck_block_1d(x, filters=None, stride=1, dilation_rate=1, name="no
     if filters is None:
         filters = x.shape[-1]
 
-    x = config.conv(x, filters, kernel_size=(3, 1), strides=(stride, 1), dilation_rate=1, use_bias=True, padding="same", name=join(name, "1", "dim0", "conv"))
-    x = config.act(x)
-    x = config.conv(x, filters, kernel_size=(1, 3), strides=(1, stride), dilation_rate=1, use_bias=True, padding="same", name=join(name, "1", "dim1", "conv"))
-    x = config.norm(x, name=join(name, "1", "norm"))
-    x = config.act(x)
+    dims = len(x.shape) - 2
 
-    x = config.conv(x, filters, kernel_size=(3, 1), strides=1, dilation_rate=(dilation_rate, 1), use_bias=True, padding="same", name=join(name, "2", "dim0", "conv"))
-    x = config.act(x)
-    x = config.conv(x, filters, kernel_size=(1, 3), strides=1, dilation_rate=(1, dilation_rate), use_bias=True, padding="same", name=join(name, "2", "dim1", "conv"))
-    x = config.norm(x, name=join(name, "2", "norm"))
+    for dim in range(dims):
+        kernel_size = [1] * dims
+        kernel_size[dim] = 3
+        strides = [1] * dims
+        strides[dim] = stride
+        x = config.conv(x, filters, kernel_size=tuple(kernel_size), strides=tuple(strides), dilation_rate=1, use_bias=True, padding="same", name=join(name, "1", f"dim{dim}", "conv"))
+        if dim == dims - 1:
+            x = config.norm(x, name=join(name, "1", "norm"))
+        x = config.act(x)
 
-    # TODO: dropout here https://github.com/Eromera/erfnet_pytorch/blob/master/train/erfnet.py#L25
+    for dim in range(dims):
+        kernel_size = [1] * dims
+        kernel_size[dim] = 3
+        dilation_rates = [1] * dims
+        dilation_rates[dim] = dilation_rate
+        x = config.conv(x, filters, kernel_size=tuple(kernel_size), strides=1, dilation_rate=tuple(dilation_rates), use_bias=True, padding="same", name=join(name, "2", f"dim{dim}", "conv"))
+        if dim == dims - 1:
+            x = config.norm(x, name=join(name, "2", "norm"))
+            # TODO: dropout here https://github.com/Eromera/erfnet_pytorch/blob/master/train/erfnet.py#L25
+            x = shortcut(x, orig_x, stride=stride, name=join(name, "shortcut"), config=config)
+        x = config.act(x)
 
-    x = shortcut(x, orig_x, stride=stride, name=join(name, "shortcut"), config=config)
-
-    x = config.act(x)
     return x
