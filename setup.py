fdaae9e0560a3d7b6f1a021c1f28014b4d800e9e
Florian Fervers <florian.fervers@t-online.de>
2021-06-21 12:22:09 +0200

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tf-semseg",
-    version="0.1.1",
+    version="0.2.0",
     python_requires=">=3",
     description="Semantic segmentation utilities for tensorflow",
     author="Florian Fervers",
