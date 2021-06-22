3e80f5efe8eecad9cb49a7a2c8c9562de934d48a
Florian Fervers <florian.fervers@gmail.com>
2021-06-22 09:15:22 +0200

    Fix assertion lower bounds

--- test/model/pretrained/test_hrnet.py
+++ test/model/pretrained/test_hrnet.py
@@ -6,11 +6,11 @@ def test_hrnet_v2_w48_cityscapes():
     predictor = lambda x: model(x, training=False)
     accuracy = eval.cityscapes(predictor, tf_semseg.model.pretrained.hrnet.hrnet_v2_w48_cityscapes.preprocess)
     print(f"Got accuracy {accuracy * 100.0}")
-    assert accuracy > 0.86
+    assert accuracy > 0.980
 
 def test_hrnet_v2_w48_ocr_cityscapes():
     model = tf_semseg.model.pretrained.hrnet.hrnet_v2_w48_ocr_cityscapes.create()
     predictor = lambda x: model(x, training=False)
     accuracy = eval.cityscapes(predictor, tf_semseg.model.pretrained.hrnet.hrnet_v2_w48_ocr_cityscapes.preprocess)
     print(f"Got accuracy {accuracy * 100.0}")
-    assert accuracy > 0.86
+    assert accuracy > 0.983
--- test/model/pretrained/test_nvidia.py
+++ test/model/pretrained/test_nvidia.py
@@ -9,10 +9,10 @@ def test_hrnet_v2_w48_ocr_mscale_cityscapes():
     predictor_singlescale = lambda x, predictor=predictor_singlescale: tf.nn.softmax(predictor(x), axis=-1)
     accuracy = eval.cityscapes(predictor_singlescale, tf_semseg.model.pretrained.nvidia.hrnet_v2_w48_ocr_mscale_cityscapes.preprocess)
     print(f"Got single-scale accuracy {accuracy * 100.0}")
-    assert accuracy > 0.87
+    assert accuracy > 0.985
 
     predictor_multiscale = tf_semseg.model.mscale.predictor_multiscale(predictor, [0.5, 1.0, 2.0], config=tf_semseg.model.pretrained.nvidia.hrnet_v2_w48_ocr_mscale_cityscapes.config)
     predictor_multiscale = lambda x, predictor=predictor_multiscale: tf.nn.softmax(predictor(x), axis=-1)
     accuracy = eval.cityscapes(predictor_multiscale, tf_semseg.model.pretrained.nvidia.hrnet_v2_w48_ocr_mscale_cityscapes.preprocess)
     print(f"Got multi-scale accuracy {accuracy * 100.0}")
-    assert accuracy > 0.87
+    assert accuracy > 0.985
