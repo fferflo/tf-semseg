3dc143002622835d15902827531a092644034710
Florian Fervers <florian.fervers@gmail.com>
2021-07-29 21:01:53 +0200

    Add intermediate layer name

--- tf_semseg/model/resnet.py
+++ tf_semseg/model/resnet.py
@@ -83,6 +83,7 @@ def resnet(x, block, num_residual_units, filters, dilation_rates, strides, name=
                     name=join(name, f"block{block_index + 1}", f"unit{unit_index + 1}"),
                     config=config,
                     **kwargs)
+        x = tf.keras.layers.Lambda(lambda x: x, name=join(name, f"block{block_index + 1}"))(x)
 
     return x
 
